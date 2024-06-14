72f385a5e4924a78d0c579ed12523c79e33adca2
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-06-14 17:44:47 -0300

    UpdateAula03Part1

--- Java-OO-Curso01/src/Exemplos/Exemplos.java
+++ Java-OO-Curso01/src/Exemplos/Exemplos.java
@@ -1,7 +1,10 @@
 package Exemplos;
 
+import java.util.Scanner;
+
 public class Exemplos {
     public static void main(String[] args) {
+        //
         String nome = "Maria";
         int idade = 30;
         double valor = 55.9999;
@@ -9,6 +12,7 @@ public class Exemplos {
 
                 println(String.format("Meu nome é %s, eu tenho %d anos e hoje gastei %.2f reais", nome, idade, valor));
 
+        //
         String nome1 = "João";
         int aulas = 4;
 
@@ -19,7 +23,53 @@ public class Exemplos {
                 """.formatted(nome1, aulas);
 
         System.out.
-
                 println(mensagem);
+
+        //
+        int dia = 3;
+        String nomeDia;
+
+        switch (dia) {
+            case 1:
+                nomeDia = "domingo";
+                break;
+            case 2:
+                nomeDia = "segunda-feira";
+                break;
+            case 3:
+                nomeDia = "terça-feira";
+                break;
+            case 4:
+                nomeDia = "quarta-feira";
+                break;
+            case 5:
+                nomeDia = "quinta-feira";
+                break;
+            case 6:
+                nomeDia = "sexta-feira";
+                break;
+            case 7:
+                nomeDia = "sábado";
+                break;
+            default:
+                nomeDia = "Dia inválido";
+                break;
+        }
+
+        System.out.println("O dia " + dia + " é " + nomeDia);
+
+        //
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome2 = scanner.nextLine();
+        System.out.print("Digite sua idade: ");
+        int idade1 = scanner.nextInt();
+        System.out.print("Digite o valor que pretende investir esse mês: ");
+        double valor1 = scanner.nextDouble();
+
+        System.out.println(nome2 + " que tem " + idade1 + " anos, irá investir R$ " + valor1 + " esse mês.");
+
+        scanner.close();
     }
 }
