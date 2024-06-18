e2c1b2805cbdbb05eb67e7018502bb67ee892bc8
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-06-18 17:21:23 -0300

    Add files via upload

---
+++ Java-OO-Curso02 - ScreenMatch/src/Principal.java
@@ -0,0 +1,16 @@
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.ducaraoEmMinutos = 180;
+
+        meuFilme.exibeFixaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.pegaMedia());
+    }
+}
