62f779df2422ae68ea0edd07253b6d16540a04cd
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-06-19 17:09:59 -0300

    Delete Java-OO-Curso02 - ScreenMatch directory

--- Java-OO-Curso02 - ScreenMatch/src/Desafio01/Pessoa.java
+++
@@ -1,9 +0,0 @@
-package Desafio01;
-
-/*1. Crie uma classe Pessoa com um método que exibe "Olá, mundo!" no console.*/
-
-public class Pessoa {
-    void exibaMensagem() {
-        System.out.println("Olá, mundo!");
-    }
-}
