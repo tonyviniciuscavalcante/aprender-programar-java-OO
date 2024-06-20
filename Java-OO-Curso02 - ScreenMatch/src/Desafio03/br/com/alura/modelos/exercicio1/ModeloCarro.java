f455085340066a8b328c0d8a91cf61385851c2fe
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-06-19 23:06:20 -0300

    AtualizandoProjeto02

---
+++ Java-OO-Curso02 - ScreenMatch/src/Desafio03/br/com/alura/modelos/exercicio1/ModeloCarro.java
@@ -0,0 +1,14 @@
+package Desafio03.br.com.alura.modelos.exercicio1;
+
+public class ModeloCarro extends Carro{
+    private int quantidadePortas;
+
+    public int getQuantidadePortas() {
+        return quantidadePortas;
+    }
+
+    public int setQuantidadePortas(int quantidadePortas) {
+        this.quantidadePortas = quantidadePortas;
+        return quantidadePortas;
+    }
+}
