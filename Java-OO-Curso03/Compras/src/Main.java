a9cd19702ac5722fd298f907391b0f83c84d88bb
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-06-21 14:29:19 -0300

    Summary

--- Java-OO-Curso03/Compras/src/Main.java
+++ Java-OO-Curso03/Compras/src/Main.java
@@ -1,5 +1,10 @@
+<<<<<<< Updated upstream
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bora!");
     }
 }
+=======
+package PACKAGE_NAME;public class Main {
+}
+>>>>>>> Stashed changes
