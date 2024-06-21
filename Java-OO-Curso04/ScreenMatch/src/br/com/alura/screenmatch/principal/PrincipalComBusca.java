d4f91fa932326d726096f79dc7431315980097ee
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-06-21 17:50:05 -0300

    Aula02 Parte01

--- Java-OO-Curso04/ScreenMatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ Java-OO-Curso04/ScreenMatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,5 +1,8 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -10,7 +13,7 @@ import java.util.Scanner;
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner leitura = new Scanner(System.in);
-        System.out.println("Digite um filme para a busca: ");
+        System.out.print("Digite um filme para a busca: ");
         var busca = leitura.nextLine();
 
         String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=44192d12";
@@ -21,6 +24,12 @@ public class PrincipalComBusca {
                 .build();
         HttpResponse<String> response = client
           .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println(meuTitulo);
     }
 }
